b9043041b0cfd849d604cfb7ed8cd9f2f8db7b79
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-03-31 16:07:18 +0300

    refactor getting iss position in func

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import requests
 from constants import *
 from datetime import datetime
 from processing import *
+
 response = requests.get(url="http://api.open-notify.org/iss-now.json", timeout=20)
 response.raise_for_status()
 
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+longitude = float(data["iss_position"]["longitude"])
+latitude = float(data["iss_position"]["latitude"])
 
 iss_position = (longitude, latitude)
 
-sunset_response = requests.get(url="https://api.sunrise-sunset.org/json", params=PARAMETERS, timeout=20)
+sunset_response = requests.get(
+    url="https://api.sunrise-sunset.org/json", params=PARAMETERS, timeout=20
+)
 sunset_response.raise_for_status()
 
 sunset_data = sunset_response.json()
--- processing.py
+++ processing.py
@@ -1,4 +1,18 @@
+import requests
+
 def hour_splitter(date_time: str) -> int:
     time_only = date_time.split("T")[1]
     hour = time_only.split(":")[0]
     return int(hour)
+
+
+def get_iss_position() -> tuple:
+    response = requests.get(url="http://api.open-notify.org/iss-now.json", timeout=20)
+    response.raise_for_status()
+
+    data = response.json()
+
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
+
+    return longitude, latitude
